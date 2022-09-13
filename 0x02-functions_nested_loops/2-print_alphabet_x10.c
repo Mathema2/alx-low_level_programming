9324610a3c1f8b11f75f4cbe2b37d3508b607fe5
Mathema2 <mathemasharon17@gmail.com>
2022-09-13 08:19:49 -0700

    Hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_alphabet -  prints alphabets 10 times in lowercase.
+* print_alphabet_x10 - prints alphabets 10 times in lowercase.
 */
 void print_alphabet_x10(void)
 {
