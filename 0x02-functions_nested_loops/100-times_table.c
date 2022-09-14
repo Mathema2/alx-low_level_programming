e5e95939df872eccf125617cc7fc8f7b6255fafc
Mathema2 <mathemasharon17@gmail.com>
2022-09-14 14:39:38 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,7 @@ for (num = 0; num <= n; ++num)
 {
 	_putchar(',');
 	_putchar(' ');
-prod + num * mult;
+prod = num * mult;
 if (prod <= 9)
  _putchar(' ');
 if (prod <= 99)
@@ -23,7 +23,7 @@ if (prod <= 99)
 
 if (prod >= 100)
 {
-	 _putchar((prod / 100)+ 48);
+	 _putchar((prod / 100) + 48);
 	 _putchar((prod / 10) % 10 + 48);
 }
 else if (prod <= 99 && prod >= 10)
