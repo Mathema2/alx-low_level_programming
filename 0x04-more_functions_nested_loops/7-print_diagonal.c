5a02d17a884a58daadd65fd18d7abf7df59107bb
Mathema2 <mathemasharon17@gmail.com>
2022-09-15 11:53:11 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal
+ * @n: the number of times the character \ should be printed
+ * Return: returns the diagonal
+ */
+
+void print_diagonal(int n)
+{
+int len;
+int space;
+
+if (n > 0)
+{
+for (len = 0; len < n; len++)
+{
+for (space = 0; space < len; space++)
+{
+	putchar(' ');
+}
+ putchar('\\');
+ if (len == (n - 1))
+{
+	continue;
+}
+ putchar('\n');
+}
+}
+}
