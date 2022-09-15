11fbc54c34861a5d2389727dca712191b4246460
Mathema2 <mathemasharon17@gmail.com>
2022-09-15 11:15:22 -0700

    Hot fix

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers except 2 and 4
+ * Return: returns the numbers
+ */
+
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+if ((i == 50) || (i == 52))
+{
+continue;
+}
+putchar(i);
+}
+putchar(10);
+}
