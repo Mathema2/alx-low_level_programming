7082224196ea3685ab4b09c5ef99bb564e05f2e4
Mathema2 <mathemasharon17@gmail.com>
2022-09-16 03:50:32 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,6 +5,7 @@
  * or fizz or buzz or fizzbuzz
  * Return: 0 (success)
  */
+
 int main(void)
 {
 int num;
@@ -12,17 +13,19 @@ int num;
 for (num = 1; num <= 100; ++num)
 {
 if ((num % 3 == 0) && !(num % 5 == 0))
-	printf("Fizz");
-else if ((num % 5 == 0 && !(num % 3 == 0))
-	 printf("Buzz");
-else if (num % 3 == 0 && num % 5 == 0)
-	printf("FizzBuzz");
+	printf ("Fizz");
+else
+if (num % 5 == 0 && !(num % 3 == 0))
+	 printf ("Buzz");
+else
+if (num % 3 == 0 && num % 5 == 0)
+	printf ("FizzBuzz");
 else
 	printf("%d", num);
 if (num != 100)
-	printf(" ");
+	printf (" ");
 else
-	printf("\n");
+	printf ("\n");
 }
 return (0);
 }
