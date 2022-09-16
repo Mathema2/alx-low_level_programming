4ed99892148482267dcefeea8c4f1124f63401ed
Mathema2 <mathemasharon17@gmail.com>
2022-09-16 04:08:24 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,16 +1,18 @@
 #include"main.h"
+
 /**
  * print_triangle - prints a triangle
  * @size: size of the triangle
  * Return: Always 0 (Success)
  */
+
 void print_triangle(int size)
 {
 int hght;
 int base;
 
 if (size <= 0)
-_putchar('\n');
+	_putchar('\n');
 else
 {
 for (hght = 1; hght <= size; ++hght)
@@ -18,9 +20,11 @@ for (hght = 1; hght <= size; ++hght)
 for (base = 1; base <= size; ++base)
 {
 if ((hght + base) <= size)
-_putchar(' ');
+	_putchar(' ');
 else
-_putchar('#');
+	_putchar('#');
 }
-_putchar('\n');																}
+	_putchar('\n');
+}
+}
 }
