23d51e912fcbea4ac46c04d2ba0adae44b50d5ec
Mathema2 <mathemasharon17@gmail.com>
2022-09-16 02:38:58 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
@@ -10,9 +10,9 @@ void print_numbers(void)
 {
 int x;
 
-for (x = 0; x <= 9; x++)
+for (x = 48; x < 58; x++)
 {
-	_putchar(x + '0');
+	_putchar(x);
 }
 _putchar('\n');
 }
