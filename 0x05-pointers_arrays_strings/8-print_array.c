3a7a98940d9893a7f9826c2da48c01127bd2fc44
Mathema2 <mathemasharon17@gmail.com>
2022-09-19 11:50:02 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: integer array to be displayed from
+ * @n: elements number to be displayed
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+int x = 0;
+
+while (x < n)
+{
+if (x == n - 1)
+	printf("%d", *(a + x));
+else
+	printf("%d, ", *(a + x));
+	x++;
+}
+printf("\n");
+}
