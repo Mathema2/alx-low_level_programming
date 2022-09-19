f44c8667991e7defe2c0f0b93fc013c25509f2be
Mathema2 <mathemasharon17@gmail.com>
2022-09-19 10:11:57 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - that reverses a string
+ * @s: pointer argument
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+int x = 0;
+
+while (*(s + x) != '\0')
+{
+	x++;
+}
+x--;
+while (x >= 0)
+{
+	_putchar(*(s + x));
+	x--;
+}
+_putchar('\n')i;
+}
