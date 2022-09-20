b79f303bb010f8ce16d800bcb0ee884ddb4229c7
Mathema2 <mathemasharon17@gmail.com>
2022-09-20 06:14:54 -0700

    Task 12

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Generates random valid passwords for the program 101-crackme.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	char m;
+
+srand(time(NULL));
+while (x <= 2645)
+{
+	m = rand() % 128;
+	x += m;
+	putchar(m);
+}
+putchar(2772 - x);
+return (0);
+}
