d6429b446728db6dc90ac0302d1a628ca74c9407
Mathema2 <mathemasharon17@gmail.com>
2022-09-22 10:22:37 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the elements of an array
+ * @a: array
+ * @n: number of elements of the array
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+int x;
+int y;
+int z;
+
+if ( n % 2 == 0)
+	z = n;
+else
+	z = n- 1;
+for (x = 0; x < z / 2; x++)
+{
+	y = a[x];
+	a[x] = a[n - 1];
+	a[n - 1] = y;
+	n--;
+}
+}
