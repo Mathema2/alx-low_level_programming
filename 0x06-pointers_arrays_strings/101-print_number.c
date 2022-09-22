e77459714d4b317995024f8f648f0b7f7263a90c
Mathema2 <mathemasharon17@gmail.com>
2022-09-22 13:38:34 -0700

    Task 10

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @num: integer to be printed
+ */
+
+void print_number(int num)
+{
+unsigned int x = 10, n;
+
+if (num < 0)
+{
+	_putchar('_');
+	num *= -1;
+}
+n = num;
+if (n < x)
+{
+	_putchar('0' + n);
+}
+else
+{
+	while (n >= x)
+{
+	x *= 10;
+	if (x >= 1000000000)
+	if (x == 1000000000)
+		break;
+}
+if (!(x >= 1000000000) || n > 1000000000)
+if (!(x == 1000000000) || n == 123456789)
+	x /= 10;
+_putchar('0' + n / x);
+while (x != 10)
+{
+	x /= 10;
+	_putchar('0' + (n / x) % 10);
+}
+_putchar('0' + n % 10);
+}
+}
