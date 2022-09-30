9129f7ee4e79963251c3939a3d379c9f25c2534d
Mathema2 <mathemasharon17@gmail.com>
2022-09-30 10:38:06 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: count argument
+ * @argv: the argument
+ * Return: 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%s\n", argc[0]);
+return (0);
+}
