0d74ffaeaa1a72c19cfbc341498ed4c7d669afcf
Mathema2 <mathemasharon17@gmail.com>
2022-10-04 13:56:01 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_array - creates an array of chars, and initializes it
+ * @size: length of the array
+ * @c: input char
+ * Return: An Array initialized with a specific chararacter
+ */
+
+char *create_array(unsigned int size, char c)
+
+{
+unsigned int a;
+char *s;
+
+s = (char *) malloc(size * sizeof(char));
+if (s == NULL)
+return (NULL);
+for (a = 0; a < size; a++)
+{
+s[a] = c;
+}
+return (s);
+}
