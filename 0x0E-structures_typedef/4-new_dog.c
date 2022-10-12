69c9ae34df9263d0e922d872801ffc519423cea3
Mathema2 <mathemasharon17@gmail.com>
2022-10-11 17:25:39 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,40 +1,73 @@
 #include "dog.h"
+#include <stdlib.h>
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- * new_dog - creates a new dog
- * @name: new dog's name
- * @age: new dog's age
- * @owner: new dog's owner
- * Return: pointer to new dog struct
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
+ * Return: The length of the string.
+ */
+
+int _strlen(char *str)
+{
+int len = 0;
+
+while (*str++)
+	len++;
+return (len);
+}
+
+/**
+ * _strcopy - Copies a string pointed to by src
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * Return: The pointer to dest.
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+int index = 0;
+
+for (index = 0; src[index]; index++)
+	dest[index] = src[index];
+dest[index] = '\0';
+return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ * Return: The new struct dog.
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
-int i;
-
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
+dog_t *doggo;
+if (name == NULL || age < 0 || owner == NULL)
 	return (NULL);
-new_dog->name = malloc(sizeof(name) + 1);
-if (new_dog->name == NULL)
+doggo = malloc(sizeof(dog_t));
+if (doggo == NULL)
+	return (NULL);
+doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (doggo->name == NULL)
 {
-	free(new_dog);
+	free(doggo);
 	return (NULL);
 }
-new_dog->owner = malloc(sizeof(owner) + 1);
-if (new_dog->owner == NULL)
+doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (doggo->owner == NULL)
 {
-	free(new_dog->name);
-	free(new_dog);
+	free(doggo->name);						
+	free(doggo);
 	return (NULL);
 }
-for (i = 0; name[i]; i++)
-	new_dog->name[i] = name[i];
-new_dog->name[i] = '\0';
-new_dog->age = age;
-for (i = 0; owner[i]; i++)
-	new_dog->owner[i] = owner[i];
-new_dog->owner[i] = '\0';
-return (new_dog);
+doggo->name = _strcopy(doggo->name, name);
+doggo->age = age;
+doggo->owner = _strcopy(doggo->owner, owner);
+return (doggo);
 }
