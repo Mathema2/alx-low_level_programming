1bc7185e2e9b6a659a9ce210025c5824ef20acd6
Mathema2 <mathemasharon17@gmail.com>
2022-10-27 10:29:55 -0700

    tasks 3-5

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * get_bit - return the value of a bit at a given index.
+ * @n: number
+ * @index: index
+ * Return: the value of the bit in index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned long int t_binary;
+
+t_binary = (sizeof(n) * 8);
+if (index < t_binary)
+{
+	return (((1 << index) & n) >> index);
+}
+return (-1);
+}
